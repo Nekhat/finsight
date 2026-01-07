5c905a7a102fe001fc4fd7bd933e08fc74951f48
Nekhat Perveen <nekhatperveen@gmail.com>
2026-01-07 15:36:10 -0800

    Add UserService interface for user registration and authentication

---
+++ src/main/java/com/finsight/service/UserService.java
@@ -0,0 +1,11 @@
+package com.finsight.service;
+
+import com.finsight.entity.User;
+
+import java.util.Optional;
+
+public interface UserService {
+    User registerUser(User user);
+    User authenticate(String email, String password);
+    Optional<User> getUserByEmail(String email);
+}
