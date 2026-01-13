d080a5bc81465d43227c1f109e348f1d8489b77d
Nekhat Perveen <nekhatperveen@gmail.com>
2026-01-13 15:07:03 -0800

    Add UserRepository for sign up and login

--- src/main/java/com/finsight/repository/UserRepository.java
+++ src/main/java/com/finsight/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.finsight.repository;
 
-public interface UserRepository {
+import com.finsight.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer>{
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
