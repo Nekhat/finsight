2512fda0c164ace8e15772f680b3a0a5b75ee07b
Nekhat Perveen <nekhatperveen@gmail.com>
2026-01-14 16:23:17 -0800

    Add user registration API with request and response DTOs

---
+++ src/main/java/com/finsight/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.finsight.controller;
+
+import com.finsight.dto.UserRegisterRequest;
+import com.finsight.dto.UserResponse;
+import com.finsight.entity.User;
+import com.finsight.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+
+    @PostMapping("/users/register")
+    public ResponseEntity<UserResponse> registerUser(
+            @RequestBody UserRegisterRequest request) {
+
+        // DTO -> Entity
+        User user = new User(
+                request.getName(),
+                request.getEmail(),
+                request.getPassword()
+        );
+
+        User savedUser = userService.registerUser(user);
+
+        // Entity -> DTO
+        UserResponse response = new UserResponse(
+                savedUser.getId(),
+                savedUser.getName(),
+                savedUser.getEmail()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+}
