e877078509d919d041b42a840fff3c1094af5c97
Nekhat Perveen <nekhatperveen@gmail.com>
2026-02-04 12:07:26 -0800

    feat(category): add CategoryRepository with duplicate-check queries

--- src/main/java/com/finsight/repository/CategoryRepository.java
+++ src/main/java/com/finsight/repository/CategoryRepository.java
@@ -1,4 +1,14 @@
 package com.finsight.repository;
 
-public interface CategoryRepository {
+import com.finsight.entity.Category;
+import com.finsight.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category,Integer> {
+    boolean existsByNameIgnoreCaseAndIsGlobalTrue(String name);
+    boolean existsByNameIgnoreCaseAndUser(String name, User user);
+    List<Category> findByIsGlobalTrueOrUser(User user);
+    boolean existsByNameIgnoreCaseAndUserAndIdNot(String name, User user, Integer id); // Does another category with this name exist for this user, excluding this category?
 }
